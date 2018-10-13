f37e04e213d2e69d5645219ebedc1eae6a51808c
cmak2 <35709929+cmak2@users.noreply.github.com>
2018-10-12 19:55:18 -0600

    Update invade.js

--- invade.js
+++ invade.js
@@ -62,9 +62,6 @@ const barrier = {
 
 /*Game startup/endgame stuff*/
 window.onload = function() {
-  update_scores();
-  //  update_scores();
- // };
   titlescreen();
   currentScreen = "title";
 };
@@ -150,14 +147,14 @@ function gameOver(){
 	else{
     ctx.strokeStyle="white";
 		ctx.strokeText("Game Over",550,90);
-		
+		ctx.strokeStyle="red";
 		ctx.strokeText("You Lost",550,200);
 	}
 	ctx.textAlign = "center";
 	ctx.strokeStyle="white";
 	ctx.strokeText(`Your Score: ${playerScore}`,550,300);
 
-  highscore(playerScore);
+
   ctx.strokeStyle="#39ff14";
   ctx.strokeRect(350,350,400,100);
   ctx.font = "4em Impact";
@@ -248,7 +245,7 @@ function update(ctx){
     win = false;
     clearInterval(upd);
     clearInterval(ast)
-    console.log('lose');
+    //console.log('lose');
     callbackClear(gameOver);
     //gameOver(won, playerScore);
   }
@@ -256,7 +253,7 @@ function update(ctx){
     win = true;
     clearInterval(upd);
     clearInterval(ast)
-    console.log('win');
+    //console.log('win');
     callbackClear(gameOver);
     //gameOver(won, playerScore);
   }
@@ -265,7 +262,7 @@ function update(ctx){
       win = false;
       clearInterval(upd);
       clearInterval(ast)
-      console.log('lose');
+      //console.log('lose');
       callbackClear(gameOver);
     }
   }
@@ -283,7 +280,7 @@ function checkCollide(ctx){
       //change shipLocation to center
 
       //return type of alien?
-      console.log('a');
+      //console.log('a');
       collide = [];
       for (i=0; i < projectileArray.length; i++){
         var type = -1;
@@ -526,7 +523,7 @@ function redrawAliens(ctx){
       for(y = 0; y < 4; y++){
         var nAlien = alienArray[x + (y * 10)];
         if(nAlien) {                                          //If exists
-          if(nAlien.alienX - 20 < 0) {
+          if(nAlien.alienX - 30 < 0) {
             sw = true;
             direction = "right";
           }
@@ -699,96 +696,4 @@ function incrementScore(type) {
       }
 }
 
-
-
-
-
-//README: We couldn't figure out how to call these functions from another file and we were running out of time, so we just settled for this.
-
-var competition_id = queryData('competition_id');
-var game_id = queryData('game_id');
-
-var current_player_score = null;
-
-function update_scores() {
-    $.ajax({
-        method: "GET",
-        url: 'https://connect.mines.edu/gameapi/gethighscores/' + competition_id + '/' + game_id,
-        dataType: 'json',
-    }).done(function(data) {
-        console.log(data);
-
-        var table = document.getElementById("hightable");
-        var count = 0;
-
-        for(var i = 0; i < data.length; i++) {
-            if(data[i]['name'] == null) {
-                $('#myhighscore').html('<td colspan="2">'+data[i]['score']+'</td>');
-                current_player_score = data[i]['score'];
-            } else {
-                var row = table.insertRow(-1);
-                var cell1 = row.insertCell(0);
-                var cell2 = row.insertCell(1);
-                cell1.innerHTML = data[i]['name'];
-                cell2.innerHTML = data[i]['score'];
-                count++;
-            }
-        }
-
-        if(count == 0) {
-            var row = table.insertRow(-1);
-            var cell = row.insertCell(0);
-            cell.colSpan = 2;
-            cell.innerHTML = 'No highscores found';
-        }
-    });
-}
-
-
-function highscore(score) {
-    var message = "";
-    if(current_player_score) {
-        if(current_player_score > score) {
-            window.alert('You already have a higher score!');
-            return;
-        }
-        message = 'Your SAVED high score is ' + current_player_score + '. ';
-    } else {
-        message = 'You have no SAVED score for this game. ';
-    }
-
-    if (confirm(message + 'Would you like to submit your new score of ' + score + ' to CS-CONNECT?')) {
-        $.ajax({
-            method: "GET",
-            url: 'https://connect.mines.edu/gameapi/submitscore/' + competition_id + '/' + game_id + '/' + score
-        }).done(function (msg) {
-            if(msg === 'success') {
-                window.alert("SUCCESS\n\nYour high score has been submitted and saved!");
-                reset_table();
-                update_scores();
-                return;
-            } else if (msg === 'competition-inactive') {
-                window.alert("Score submission failed... competition is inactive.");
-                return;
-            }
-
-            window.alert("ERROR\n\nYour high score was unable to be saved!");
-        });
-
-    }
-}
-
-function reset_table(){
-    $('#hightable').html('<tr><th>User</th><th>Score</th></tr>');
-    $('#myhightable').html('<table id="myhightable"><tr><th colspan="2">Your High Score</th></tr><tr id="myhighscore"><td colspan="2">You dont have one yet!</td></tr></table>');
-}
-
-function queryData(variable) {
-    var data = window.location.search.substring(1)
-    data = data.split('&');
-    for (var i = 0; i < data.length; i++) {
-        var subdata = data[i].split('=');
-        if(subdata[0] == variable){return subdata[1]};
-    }
-    return false;
-}
+//need to change array to hold alien x and y position instead of 0/1, negative 1 vals can signify the alien is dead
